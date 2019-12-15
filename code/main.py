851345c97d79109488caa34f5714a851e769548e
Laksh <lakshyajeet22@gmail.com>
2019-12-15 20:03:53 +0530

    Add CSV reading function and point drawing function

--- code/main.py
+++ code/main.py
@@ -1,56 +1,25 @@
+import time
 import cv2
 import numpy as np
-from yolo_detector import yolo_detect
-from haar_cascade_detector import haar_detect
-from blob_detection import blob_detect
 
-from background_extraction import extract_background
-from foreground_extraction import get_foreground
-from road_mask_creation import *
+from utilities.to_df import to_df, to_coordinates
+
+df = to_coordinates("../data/detections.txt")
 
 capture = cv2.VideoCapture('../data/aic19-track3-train-data/2.mp4')
 
-subtractor = cv2.createBackgroundSubtractorMOG2()
+_, img = capture.read()
 
-params = cv2.SimpleBlobDetector_Params()
-params.filterByArea = True
-params.minArea = 10
-params.maxArea = 1000000
-params.filterByColor = True
-params.blobColor = 255
-blob_detector = cv2.SimpleBlobDetector_create(params)
+coordinate_frame = np.zeros((img.shape[:2][0],img.shape[:2][1],3), np.uint8)
 
-_, frame = capture.read()
-average = np.float32(frame)
-
-# First we create the road mask
-mask = get_road_mask('../data/aic19-track3-train-data/3.mp4', 1000)
-mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-
-while True:
+for index, row in df.iterrows():
     _, frame = capture.read()
 
-    # Background Extraction
-    #background, average = extract_background(frame, average)
-    #cv2.imshow("Background", background)
-    # Foreground Extraction
-    # TODO: Overlay onto actual frame
-    #foreground = get_foreground(frame, subtractor)
-    #cv2.imshow("Foreground", foreground)
-    # Road Mask
-    #driveable_area = cv2.bitwise_and(mask, frame)
-    #cv2.imshow("Driveable Area", driveable_area)
+    cv2.circle(coordinate_frame, (int(row.x/2), int(row.y/2)), 2, (0, 0, 255), 2)
 
-    #=== DETECTION METHODS ===#
-    # Detection using YOLO
-    yolo_detections = yolo_detect(frame)
-    cv2.imshow("YOLO Detections", yolo_detections)
-    # Detecting using Haar Cascade
-    #haar_detections = haar_detect(frame)
-    #cv2.imshow("Haar Detections", haar_detections)
-    # Detection using Blob Detector
-    #blob_detections = blob_detect(frame, foreground, blob_detector)
-    #cv2.imshow("Blob Detections", blob_detections)
+    cv2.imshow("Video", cv2.bitwise_or(frame, coordinate_frame))
+
+    print(row["id"], row['x'], row['y'])
 
     key = cv2.waitKey(30) & 0xff
     if key == 27:
@@ -58,3 +27,6 @@ while True:
 
 capture.release()
 cv2.destroyAllWindows()
+
+
+    
---
+++ code/utilities/to_df.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Convert text file of detected objects to a pandas dataframe
+def to_df(path):
+    df = pd.read_csv(path, index_col=0, header=None)
+    df = df.drop([6, 7], axis=1)
+    df.columns = ["id", "left", "top", "width", "height"]
+    return df
+
+# Convert default top, left, height, width to locations of centroids
+def to_coordinates(path):
+    df = to_df(path)
+    df['x'] = df.apply(lambda row: row.left + row.width/2, axis=1)
+    df['y'] = df.apply(lambda row: row.top + row.height/2, axis=1)
+    df = df.drop(["top", "left", "width", "height"], axis=1)
+    return df
